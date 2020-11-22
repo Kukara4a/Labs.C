47ccd697410e11095203c5f77b7e2fcf8b8b1f5e
Kukara4a <ilya_nesterov_01@mail.ru>
2020-11-22 18:28:33 +0500

    Final commit

---
+++ Maze/Maze/main.cpp
@@ -0,0 +1,29 @@
+#include "Maze.h"
+
+Maze* buildingLadder();
+
+int main()
+{
+	auto mazeLadder = buildingLadder();
+	mazeLadder->printMaze();	
+}
+
+Maze* buildingLadder()
+{
+	auto maze = new Maze(5, 5);
+	int x = 0;
+	int y = 1;
+	for (int i = 0; i < 5; i++)
+	{
+		for (int j = 0; j < 5; j++)
+		{
+			if (j == x)
+				maze->makeConnection(i, j, i, j + 1);
+			if (j == y)
+				maze->makeConnection(i, j, i + 1, j);
+		}
+		x = x + 1;
+		y = y + 1;
+	}
+	return maze;
+}
